862447a2982f9e7e83a8fcfde382359f3f0fe57a
claudiu <claudiu.garba@gmail.com>
2015-11-28 19:57:10 +0100

    when

--- src/basic/basic.kt
+++ src/basic/basic.kt
@@ -13,4 +13,31 @@ fun main(args: Array<String>) {
     println(nr)
     println(value1)
     println(stop)
+    "Claudiu".toCharArray().forEach { println(it) }
+
+    val array = Array(5, { i -> (i * i).toString() })
+    println(array)
+
+    val str = "cAldu"
+    println(str)
+
+    if (nr > 10) {
+        println(nr)
+    } else {
+        println("nn")
+    }
+
+    when(nr) {
+        10 -> print("10")
+        2, 101 -> print("121")
+        100 -> print("number")
+
+    }
+    val nn = "claudiu"
+    when(nn) {
+        is String -> nn.startsWith("c")
+        else -> false
+    }
+
+
 }
