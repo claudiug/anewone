d247279ad7aa5321e07896badf46a5a9551c6446
claudiu <claudiu.garba@gmail.com>
2015-11-28 19:38:05 +0100

    Initial commit

---
+++ src/basic/basic.kt
@@ -0,0 +1,16 @@
+package basic
+
+/**
+ * Created by claudiug on 11/28/15.
+ */
+
+fun main(args: Array<String>) {
+
+    val nr = 100
+    val value1: Int = 100
+    val stop: Double = 1000.00
+
+    println(nr)
+    println(value1)
+    println(stop)
+}
